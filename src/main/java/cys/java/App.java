b8b0c6d4e429fc303b3a845ce49516e3feaf3c09
mdelaiglesia <matiasdelaiglesia@gmail.com>
2014-11-18 00:44:34 -0300

    Primer commit

---
+++ src/main/java/cys/java/App.java
@@ -0,0 +1,13 @@
+package cys.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
